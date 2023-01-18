06f76548b43789d30432c233d47dd5ad90ee1f78
ssRahul-github <104823570+ssRahul-github@users.noreply.github.com>
2023-01-18 18:50:35 +0530

    Add files via upload

---
+++ EMS/src/main/java/com/greatlearning/ems/entity/Employee.java
@@ -0,0 +1,65 @@
+package com.greatlearning.ems.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "employees")
+public class Employee {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long Id;
+	@Column(name = "first_name")
+	private String firstname;
+	@Column(name = "last_name")
+	private String lastname;
+	@Column(name = "email")
+	private String email;
+
+	public Employee() {
+	}
+
+	public Employee(String firstName, String lastName, String email) {
+
+		this.firstname = firstName;
+		this.lastname = lastName;
+		this.email = email;
+	}
+
+	public Long getId() {
+		return Id;
+	}
+
+	public void setId(Long id) {
+		this.Id = id;
+	}
+
+	public String getFirstName() {
+		return firstname;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstname = firstName;
+	}
+
+	public String getLastName() {
+		return lastname;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastname = lastName;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+}
---
+++ EMS/src/main/java/com/greatlearning/ems/repository/EmployeeRepository.java
@@ -0,0 +1,11 @@
+package com.greatlearning.ems.repository;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.greatlearning.ems.entity.Employee;
+
+public interface EmployeeRepository extends JpaRepository<Employee, Long> {
+
+}
